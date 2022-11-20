5c018c1da4b7284b49ed5e34495cf357686e1268
MargaretBaranovskaya <95936987+margokroshka@users.noreply.github.com>
2022-11-20 19:47:15 +0300

    Add files via upload

---
+++ lesson22/src/main/java/MyFirstServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.lang.System.out;
+
+@WebServlet("/time")
+public class MyFirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter= resp.getWriter();
+        Date dNow = new Date( );
+        printWriter.println(dNow);
+
+
+    }
+}
